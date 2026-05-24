3e3a9490d1e5ddac4673b1625781c6a180e97c28
User <user@example.com>
2026-05-24 19:23:38 +0800

    Updates

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const CACHE = 'expense-tracker-v2';
+const CACHE = 'expense-tracker-v3';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
