270304eabc062e2c7ff9f947d4030e57d4996ced
User <user@example.com>
2026-05-24 18:51:13 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'expense-tracker-v2';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((r) =>
+      r || fetch(e.request).then((res) => {
+        if (res.ok && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, clone));
+        }
+        return res;
+      })
+    )
+  );
+});
